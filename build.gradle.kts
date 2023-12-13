1488cdfc9ecfaa89c73936f97c7648964f51ad7d
thegr8manef <jedidimanef@gmail.com>
2023-12-13 16:10:19 +0100

    tag 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,24 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
+    id("com.android.library").version("7.4.1").apply(false)
+    kotlin("multiplatform").version("1.8.0").apply(false)
     id("maven-publish")
-    kotlin("native.cocoapods") version "1.9.21"
+
 
 }
-buildscript {
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.1.4") // Use the appropriate version
-    }
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
+
+//plugins {
+//    //trick: for the same plugin versions in all sub-modules
+//    alias(libs.plugins.androidLibrary).apply(false)
+//    alias(libs.plugins.kotlinMultiplatform).apply(false)
+//    id("maven-publish")
+//}
+//buildscript {
+//    dependencies {
+//        classpath ("com.android.tools.build:gradle:8.2.0") // Use the appropriate version
+//    }
+//}
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,104 +1,182 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-
+//import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+    kotlin("multiplatform")
+    kotlin("native.cocoapods")
+    id("com.android.library")
     id("maven-publish")
-    kotlin("native.cocoapods") version "1.9.21"
 }
 
 kotlin {
-    val xcf = XCFramework()
-    jvm()
-    js()
-    mingwX64()
-    linuxX64()
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "19"
+            }
+        }
+    }
     iosX64()
     iosArm64()
     iosSimulatorArm64()
-    val publicationsFromMainHost =
-        listOf(jvm(), js()).map { it.name } + "kotlinMultiplatform"
-    publishing {
-        publications {
-            matching { it.name in publicationsFromMainHost }.all {
-                val targetPublication = this@all
-                tasks.withType<AbstractPublishToMaven>()
-                    .matching { it.publication == targetPublication }
-                    .configureEach { onlyIf { findProperty("isMainHost") == "true" } }
-            }
-        }
-    }
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
-        }
-        android{
-            publishLibraryVariants("release", "debug")
-            publishLibraryVariantsGroupedByFlavor = true
-        }
-    }
-    
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared"
-            isStatic = true
-            xcf.add(this)
+
+    cocoapods {
+        summary = "LibKMM"
+        homepage = "https://github.com/thegr8manef/LibKMM"
+        version = "2.0.0"
+        authors = "Thegr8manef, Manef Jedidi"
+        license = "Apache-2.0"
+        ios.deploymentTarget = "14.1"
+        framework {
+            baseName = "LibKMM"
         }
     }
 
     sourceSets {
-        commonMain.dependencies {
-            //put your multiplatform dependencies here
-            //classpath ("com.android.tools.build:gradle:7.0.0") // Use the appropriate version
-
+        val ktor_version = "2.2.4"
+        val commonMain by getting {
+            dependencies {
+                //implementation("io.ktor:ktor-client-core:$ktor_version")
+            }
         }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                //implementation("io.ktor:ktor-client-okhttp:$ktor_version")
+            }
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                //implementation("io.ktor:ktor-client-ios:$ktor_version")
+            }
         }
     }
-    cocoapods {
-        // Required properties
-        // Specify the required Pod version here. Otherwise, the Gradle project version is used.
-        version = "1.1"
-        summary = "Some description for a Kotlin/Native module"
-        homepage = "Link to a Kotlin/Native module homepage"
-
-        // Optional properties
-        // Configure the Pod name here instead of changing the Gradle project name
-        name = "LibKMM"
-
-        framework {
-            // Required properties
-            // Framework name configuration. Use this property instead of deprecated 'frameworkName'
-            baseName = "LibKMM"
-
-            // Optional properties
-            // Specify the framework linking type. It's dynamic by default.
-            isStatic = false
-            // Dependency export
-            export(project(":shared"))
-            transitiveExport = false // This is default.
-            // Bitcode embedding
-            embedBitcode(org.jetbrains.kotlin.gradle.plugin.mpp.BitcodeEmbeddingMode.BITCODE)
-        }
-
-        // Maps custom Xcode configuration to NativeBuildType
-        xcodeConfigurationToNativeBuildType["CUSTOM_DEBUG"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.DEBUG
-        xcodeConfigurationToNativeBuildType["CUSTOM_RELEASE"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.RELEASE
-    }
-
 }
 
 android {
     namespace = "com.example.libkmm"
-    compileSdk = 34
+    compileSdk = 33
     defaultConfig {
-        minSdk = 24
+        minSdk = 27
+        targetSdk = 33
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_19
+        targetCompatibility = JavaVersion.VERSION_19
     }
 }
+//
+//plugins {
+//    alias(libs.plugins.kotlinMultiplatform)
+//    alias(libs.plugins.androidLibrary)
+//    id("maven-publish")
+//    kotlin("native.cocoapods") version "1.9.21"
+//}
+//
+//kotlin {
+//    val xcf = XCFramework()
+//    jvm()
+//    js()
+//    mingwX64()
+//    linuxX64()
+//    iosX64()
+//    iosArm64()
+//    iosSimulatorArm64()
+//    val publicationsFromMainHost =
+//        listOf(jvm(), js()).map { it.name } + "kotlinMultiplatform"
+//    publishing {
+//        publications {
+//            matching { it.name in publicationsFromMainHost }.all {
+//                val targetPublication = this@all
+//                tasks.withType<AbstractPublishToMaven>()
+//                    .matching { it.publication == targetPublication }
+//                    .configureEach { onlyIf { findProperty("isMainHost") == "true" } }
+//            }
+//        }
+//    }
+//    androidTarget {
+//        compilations.all {
+//            kotlinOptions {
+//                jvmTarget = "1.8"
+//            }
+//        }
+//        android{
+//            publishLibraryVariants("release", "debug")
+//            publishLibraryVariantsGroupedByFlavor = true
+//        }
+//    }
+//
+//    listOf(
+//        iosX64(),
+//        iosArm64(),
+//        iosSimulatorArm64()
+//    ).forEach {
+//        it.binaries.framework {
+//            baseName = "shared"
+//            isStatic = true
+//            xcf.add(this)
+//        }
+//    }
+//
+//    sourceSets {
+//        commonMain.dependencies {
+//            //put your multiplatform dependencies here
+//            //classpath ("com.android.tools.build:gradle:7.0.0") // Use the appropriate version
+//
+//        }
+//        commonTest.dependencies {
+//            implementation(libs.kotlin.test)
+//        }
+//    }
+//    cocoapods {
+//        // Required properties
+//        // Specify the required Pod version here. Otherwise, the Gradle project version is used.
+//        version = "1.1"
+//        summary = "Some description for a Kotlin/Native module"
+//        homepage = "Link to a Kotlin/Native module homepage"
+//
+//        // Optional properties
+//        // Configure the Pod name here instead of changing the Gradle project name
+//        name = "LibKMM"
+//
+//        framework {
+//            // Required properties
+//            // Framework name configuration. Use this property instead of deprecated 'frameworkName'
+//            baseName = "LibKMM"
+//
+//            // Optional properties
+//            // Specify the framework linking type. It's dynamic by default.
+//            isStatic = false
+//            // Dependency export
+//            export(project(":shared"))
+//            transitiveExport = false // This is default.
+//            // Bitcode embedding
+//            embedBitcode(org.jetbrains.kotlin.gradle.plugin.mpp.BitcodeEmbeddingMode.BITCODE)
+//        }
+//
+//        // Maps custom Xcode configuration to NativeBuildType
+//        xcodeConfigurationToNativeBuildType["CUSTOM_DEBUG"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.DEBUG
+//        xcodeConfigurationToNativeBuildType["CUSTOM_RELEASE"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.RELEASE
+//    }
+//
+//}
+//
+//android {
+//    namespace = "com.example.libkmm"
+//    compileSdk = 34
+//    defaultConfig {
+//        minSdk = 24
+//    }
+//}
